d35f773922fe8ff715c76966706244e17778b7c5
Cole Fritz Culler <cbroski.culler@gmail.com>
2015-05-13 11:44:16 -0700

    Thruster Audio
    The thruster audio now works when you move forward and backwards. Right
    now the audio just loops, which is pretty annoying.
    
    If you press x, it kills the engines, stops the ship and stops the
    audio.

--- StopMovingAction.java
+++ StopMovingAction.java
@@ -6,7 +6,7 @@ import sage.camera.*;
 import graphicslib3D.Vector3D;
 import graphicslib3D.Point3D;
 
-public class MoveForwardAction extends AbstractInputAction
+public class StopMovingAction extends AbstractInputAction
 {
 	private ICamera camera;
 	private SpaceShip ship;
@@ -14,7 +14,7 @@ public class MoveForwardAction extends AbstractInputAction
 	private MyClient game;
 	private MyNetworkingClient mnc;
 	
-	public MoveForwardAction(MyNetworkingClient mnc, MyClient game, ICamera c, SpaceShip ship)
+	public StopMovingAction(MyNetworkingClient mnc, MyClient game, ICamera c, SpaceShip ship)
 	{
 		this.mnc = mnc;
 		this.game = game;
@@ -26,8 +26,8 @@ public class MoveForwardAction extends AbstractInputAction
 	public void performAction(float speed, Event e)
 	{
 		if(ship.getSpeed() < 5){		//Set a speed limit for aircraft
-			ship.setSpeed(ship.getSpeed() + 0.25f);
-			mnc.playThrustersSound();
+			ship.setSpeed(ship.getSpeed()*0);
+			mnc.stopThrustersSound();
 		}
 		Point3D loc = ship.getLocation();
 		Vector3D newLoc = new Vector3D(loc.getX(), loc.getY(), loc.getZ());
