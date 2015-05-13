8b58f7298d868bc0f339b471589f0e2f224f85c3
Cole Fritz Culler <cbroski.culler@gmail.com>
2015-05-13 12:53:47 -0700

    TestServer Cleanup
    Cleaning up TestServer: removing comments and fixing bracket alignment.

--- TestServer.java
+++ TestServer.java
@@ -4,22 +4,13 @@ import java.io.IOException;
 import java.net.InetAddress;
 
 public class TestServer
-	{
-		GameServerTCP server;
-		//private static GameServerTCP server;
-		
-			//private static IClientInfo IClientInfo;
+{
+	GameServerTCP server;
 
-			public static <K> void main(String[] args) throws IOException
-			{ 
-				System.out.println("Server address:" + InetAddress.getLocalHost());
-				GameServerTCP server = new GameServerTCP(10001);
+	public static <K> void main(String[] args) throws IOException
+	{ 
+		System.out.println("Server address:" + InetAddress.getLocalHost());
+		GameServerTCP server = new GameServerTCP(10001);
 				
-			}
-			
-		
-//Here is a comment!!!
-			//HERE IS ANOTHER COMMENT
-			
-
 	}
+}
